5c37fd2028f5c00dbeb86f12776a79d1a31e1678
Rick Kock <rit.kock@gmail.com>
2024-04-02 20:36:08 +0200

    TDD project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,14 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'personal-task-manager';
+
+  constructor() {}
+
+  doSomething() {
+    console.log('Log something');
+  }
 }
